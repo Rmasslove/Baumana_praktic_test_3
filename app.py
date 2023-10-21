af33255ef84f067586565ca610ce1fa0a993f1be
Rmasslove <r.masslove@gmail.com>
2023-10-21 17:10:23 +0700

    Первый коммит

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def print_hello():
+    return render_template('index.html')
+
+
+@app.route('/text/')
+def print_text():
+    return "Some text!"
+
+
+app.run()
