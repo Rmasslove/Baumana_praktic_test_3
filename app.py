ccde7cd1886e3cb7d1695d1359bd138c4cefe038
Rmasslove <r.masslove@gmail.com>
2023-10-22 11:29:49 +0700

    Восьмой коммит (добавлен scaler)

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 from flask import Flask, render_template, request
-import tensorflow as tf
-
 import pickle
-import sklearn
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.preprocessing import MinMaxScaler
-
 
 
 app = Flask(__name__)
@@ -13,29 +7,31 @@ app = Flask(__name__)
 
 @app.route('/', methods=['get', 'post'])
 def predict():
+
     message = ''
+
     if request.method == 'POST':
-        iw_q = request.form.get('pole1')
-        if_q = request.form.get('pole2')
-        vw_q = request.form.get('pole3')
-        fp_q = request.form.get('pole4')
+        iw_q = request.form.get('iw_q')
+        if_q = request.form.get('if_q')
+        vw_q = request.form.get('vw_q')
+        fp_q = request.form.get('fp_q')
 
         request_on_predict = [[float(iw_q), float(if_q), float(vw_q), float(fp_q)]]
-
 
         with open('model/ebw_mlp.pkl', 'rb') as file:
             loaded_model = pickle.load(file)
 
+        with open('model/scaler_mlp.pkl', 'rb') as sc:
+            scaler = pickle.load(sc)
 
-        pred = loaded_model.predict(request_on_predict)
-
+        pr_sc = scaler.transform(request_on_predict)
+        pred = loaded_model.predict(pr_sc)
 
         message = f'Depth и Width {pred[0][1], pred[0][0]}'
 
-        print(iw_q, if_q, vw_q, fp_q, 'predict', pred)
+        print(iw_q, if_q, vw_q, fp_q, pred, pr_sc, scaler)
 
     return render_template('index.html', message=message)
-
 
 
 app.run()  # Закоментировать перед развёртыванием на сайте.
