02a9319ead6e3e4a23d2dc330e3b750d78a6898b
Rmasslove <r.masslove@gmail.com>
2023-10-22 11:08:26 +0700

    Седьмой коммит (подключена линейная модель)

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 import tensorflow as tf
 
+import pickle
+import sklearn
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.preprocessing import MinMaxScaler
 
 
 
@@ -16,27 +20,22 @@ def predict():
         vw_q = request.form.get('pole3')
         fp_q = request.form.get('pole4')
 
-        request_on_predict = [[float(iw_q), float(if_q), float(vw_q), float(fp_q), 8., 6.]]
+        request_on_predict = [[float(iw_q), float(if_q), float(vw_q), float(fp_q)]]
 
-        # pkl_ebw_mlp = "model\\ebw_mlp.txt"
-        # with open(pkl_ebw_mlp, 'rb') as file:
-        #     pickle_model = pickle.load(file)
 
-        # model = joblib.load("model.joblib")
+        with open('model/ebw_mlp.pkl', 'rb') as file:
+            loaded_model = pickle.load(file)
 
-        model_loaded = tf.keras.models.load_model("model\\titanic_mlp")
 
-        pred = model_loaded.predict(request_on_predict)
-        message = f'Depth и Width {pred}'
-        # message = f'Depth и Width {pred[0][1], pred[0][0]}'
+        pred = loaded_model.predict(request_on_predict)
+
+
+        message = f'Depth и Width {pred[0][1], pred[0][0]}'
+
         print(iw_q, if_q, vw_q, fp_q, 'predict', pred)
 
     return render_template('index.html', message=message)
 
-
-# @app.route('/text/')  # Другая страница
-# def print_text():
-#     return "Some text!"
 
 
 app.run()  # Закоментировать перед развёртыванием на сайте.
